10a06c635417baf0abd26a5df9b51ea0785e47be
Sridhar Nallasamy <nsridharbtech@gmail.com>
2024-04-29 22:17:16 +0530

    feature pipeline test 26

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
             target='_blank'
             rel='noopener noreferrer'
           >
-            Hi{' '}
+            HioO{' '}
             <Image
               src='/vercel.svg'
               alt='Vercel Logo'
